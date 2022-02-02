97bb273cd9b1a4395de15ede9973f55bebb2b706
rrryuutei <48124924+rrryuutei@users.noreply.github.com>
2022-02-01 22:19:12 -0800

    Update MajorityElementII.java

--- Assignment1/src/MajorityElementII.java
+++ Assignment1/src/MajorityElementII.java
@@ -3,24 +3,23 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MajorityElementII {
-
-     public List<Integer> majorityElement(int[] nums) {
-            Arrays.sort(nums);
-            List<Integer>res=new ArrayList<>();
-            for(int i=0;i<nums.length;i++)
-            {
-                int j=i;
-                while(j<nums.length && nums[j]==nums[i])
-                    j++;
-                if(j-i>(nums.length)/3)
-                    res.add(nums[i]);
-                i=j-1;
-
-            }
-            return res;
-
+  class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        List<Integer>res=new ArrayList<>(); 
+        for(int i=0;i<nums.length;i++)//
+        {
+            int j=i;
+            while(j<nums.length && nums[j]==nums[i])
+                j++;
+            if(j-i>(nums.length)/3) 
+                res.add(nums[i]);
+            i=j-1;
+            
         }
-
-
+        return res;
+        
+    }
+}
 
 }
