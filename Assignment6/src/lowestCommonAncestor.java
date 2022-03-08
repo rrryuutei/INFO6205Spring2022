4be588a1d3e7798201030931f6846c7637dabb46
rrryuutei <48124924+rrryuutei@users.noreply.github.com>
2022-03-07 20:29:45 -0800

    Update lowestCommonAncestor.java

--- Assignment6/src/lowestCommonAncestor.java
+++ Assignment6/src/lowestCommonAncestor.java
@@ -1,25 +1,26 @@
 public class lowestCommonAncestor {
-    class Solution {
-        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-            if(root.val == p.val || root.val == q.val) {
+   
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root.val == p.val || root.val == q.val) {
                 return root;
-            }
-            if((!isz(root.left, p) || !isz(root.left, q))) {
-                return lowestCommonAncestor(root.left, p, q);
-            }
-            if((!isz(root.right,p) || !isz(root.right,q))) {
+        }
+        if((isz(root.left, p) &&isz(root.left, q))) { 
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        if((isz(root.right,p) &&isz(root.right,q))) {
                 return lowestCommonAncestor(root.right, p, q);
-            }
-            return root;
         }
-        public boolean isz(TreeNode root,TreeNode p){
-            if(root == null){
-                return false;
-            }
-            if(root.val == p.val){
-                return true;
-            }
-            return isz(root.left, p) || isz(root.right, p);
-        }
+        return root;
     }
+    public boolean isz(TreeNode root,TreeNode p){ 
+        if(root == null){ 
+                return false;
+        }
+        if(root.val == p.val){
+                return true;
+        }
+        return isz(root.left, p) || isz(root.right, p);
+    }
+}
 }
