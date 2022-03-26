fc4e3173410004e9999f639dfb67134ff7d3713e
rryuutei <sixting19@outlook.com>
2022-03-26 12:25:58 -0700

    midterm

--- Midterm/src/Question4.java
+++ Midterm/src/Question4.java
@@ -4,32 +4,40 @@ import java.util.Comparator;
 import java.util.List;
 
 public class Question4{
-class solution {
-    public int[][] merge(int[][] arr) {
-        if(arr == null || arr.length<=1)
-            return arr;
-        List<int[]> list = new ArrayList<>();
-        Arrays.sort(arr,new Comparator<int[]>(){
-            @Override
-            public int compare(int[] nums1,int[] nums2){
-                return nums1[0]-nums2[0];
+    class Solution {
+        public int[][] merge(int[][] a) {
+            int n=a.length;
+            int count=0;
+            if(n==0)
+                return new int[][]{};
+            for(int i=0;i<n-1;i++){
+                if(a[i]==null)
+                    continue;
+                int s=a[i][0],t=a[i][a[i].length-1];
+                for(int j=i+1;j<n;j++){
+                    if(a[j]!=null){
+                        int p=a[j][0],q=a[j][a[j].length-1];
+                        if((p<=t&&p>=s)||(s>=p&&s<=q))
+                        {
+                            count++;
+                            a[j][0]=Math.min(a[j][0],a[i][0]);
+                            a[j][a[j].length-1]=Math.max(a[j][a[j].length-1],a[i][a[i].length-1]);
+                            a[i]=null;
+                            break;
+                        }
+                    }
+                }
             }
-        });
-        int i=0;
-        int n = arr.length;
-        while(i<n){
-            int left = arr[i][0];
-            int right = arr[i][1];
-            while(i<n-1 && right>=arr[i+1][0]){
-                right = Math.max(right,arr[i+1][1]);
-                i++;
+            int[][] res=new int[n-count][2];
+            int m=0;
+            for(int i=0;i<a.length;i++){
+                if(a[i]!=null)
+                    res[m++]=a[i];
             }
-            list.add(new int[] {left,right});
-            i++;
+            return res;
         }
-        return list.toArray(new int[list.size()][2]);
     }
-}
+
 }
 
 
